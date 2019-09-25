fc13da19ca65e2e9d260d76dd456ad297204e833
kenwhiston <kenwhistonuns2005@gmail.com>
2019-09-25 16:44:44 -0500

    ajustes menores

--- lib/Principal.dart
+++ lib/Principal.dart
@@ -58,22 +58,24 @@ class _HomePrincipalState extends State<HomePrincipal> {
         title: Text(widget.title),
       ),
       body:
-        Center(
-          child: Container(
-            height: 200.0,
-            width: 200.0,
-            child: RaisedButton(
-              onPressed: () {
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text("HELLO!"),
-                ));
-              },
-              child: Text("HOLA"),
-              color: Colors.redAccent,
-              textColor: Colors.white,
+        Builder(builder: (BuildContext context){
+          return Center(
+            child: Container(
+              height: 200.0,
+              width: 200.0,
+              child: RaisedButton(
+                onPressed: () {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text("HELLO!"),
+                  ));
+                },
+                child: Text("HOLA"),
+                color: Colors.redAccent,
+                textColor: Colors.white,
+              ),
             ),
-          ),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        }),
     );
   }
 }
